87f4ca1cf2363a32c0015000619b9f6499c8a37e
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-03-07 08:06:40 -0800

    Replace literals with variables.

--- examples/plot_vep_elevation.R
+++ examples/plot_vep_elevation.R
@@ -1,16 +1,25 @@
 #! /usr/bin/env Rscript
 
 library(FedData)
-library(magrittr)
 
 # Extract data for the Village Ecodynamics Project "VEPIIN" study area:
 # http://veparchaeology.org
 vepPolygon <- polygon_from_extent(raster::extent(672800,740000,4102000,4170000),
-                                  proj4string="+proj=utm +datum=NAD83 +zone=12")
+                                  proj4string='+proj=utm +datum=NAD83 +zone=12')
 
-# Get the NED (USA ONLY)
-# Returns a raster
-NED <- get_ned(template=vepPolygon,  label="VEPIIN")
+run.label = 'vepiin'
+run.base <- 'plot_vep_elevation_run'
+ned.resolution <- '1'
+ned.raw.dir <- paste0(run.base, '/raw/', 'ned')
+ned.extraction.dir <- paste0(run.base, '/extracted/', run.label, 'ned')
 
-png('vep_elevation.png')
-raster::plot(NED)
+ned <- get_ned(template=vepPolygon,
+               label=run.label, 
+               res=ned.resolution,
+               raw.dir=ned.raw.dir,
+               extraction.dir=ned.extraction.dir
+)
+
+ned.plot.filename <- paste0(run.base, '/', run.label, '_ned.pdf')
+pdf(ned.plot.filename)
+raster::plot(ned)
